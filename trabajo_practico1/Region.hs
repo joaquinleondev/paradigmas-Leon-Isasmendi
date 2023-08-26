25b7db18d735e2fe776fab0bb3a01ef787629386
Joaquin <joaquinleondev@gmail.com>
2023-08-26 19:19:51 -0300

    Final upload

--- trabajo_practico1/Region.hs
+++ trabajo_practico1/Region.hs
@@ -27,33 +27,40 @@ tunelR :: Region -> [ City ] -> Region
 tunelR (Reg cities links tunnels) citiesToAdd
     | length cities < 2 = error "Debe haber al menos dos ciudades"
     | any (`notElem` cities) citiesToAdd = error "Alguna de las ciudades no existe"
-    | not (validTunelR (Reg cities links tunnels) citiesToAdd 0) = error "No hay links que unan las dos ciudades"
+    | not (validTunelR (Reg cities links tunnels) citiesToAdd 0) = error "No hay links que unan las dos ciudades o con capacidad suficiente"
     | otherwise = Reg cities links (newT (citiesToLinksR (Reg cities links tunnels) citiesToAdd):tunnels)
 
 
 connectedR :: Region -> City -> City -> Bool
-connectedR (Reg _ _ tunnels) city1 city2 = any (connectsT city1 city2) tunnels
+connectedR (Reg cities _ tunnels) city1 city2
+    | not (city1 `elem` cities && city2 `elem` cities) = error "Alguna de las ciudades no existe"
+    | otherwise = any (connectsT city1 city2) tunnels
 
 
 linkedR :: Region -> City -> City -> Bool
-linkedR (Reg _ links _) city1 city2 = any (linksL city1 city2) links
+linkedR (Reg cities links _) city1 city2 
+    | not (city1 `elem` cities && city2 `elem` cities) = error "Alguna de las ciudades no existe"
+    | otherwise = any (linksL city1 city2) links
 
 
 delayR :: Region -> City -> City -> Float
 delayR (Reg cities links tunnels) city1 city2
-    | not (city1 `elem` cities && city2 `elem` cities) = error "Las ciudades no existen"
+    | not (city1 `elem` cities && city2 `elem` cities) = error "Alguna de las ciudades no existe"
     | not (connectedR (Reg cities links tunnels) city1 city2) = error "Las ciudades no estan conectadas"
     | otherwise = delayT (head (filter (connectsT city1 city2) tunnels))
 
 
 availableCapacityForR :: Region -> City -> City -> Int
-availableCapacityForR = error "Not implemented yet"
+availableCapacityForR (Reg cities links tunnels) city1 city2
+    | not (city1 `elem` cities && city2 `elem` cities) = error "Alguna de las ciudades no existe"
+    | otherwise = foldr (\link fold -> fold + (capacityLinkR tunnels link)) 0 (filter (linksL city1 city2) links)
 
 
 validTunelR :: Region -> [ City ] -> Int -> Bool
-validTunelR (Reg cities links tunnels) citiesToAdd index 
+validTunelR (Reg cities links tunnels) citiesToAdd index
+    | any (\tunel -> connectsT (head citiesToAdd) (last citiesToAdd) tunel) tunnels = error "Ya existe un tunel entre las ciudades"
     | index == length citiesToAdd - 1 = True
-    | any (\link -> linksL (citiesToAdd !! index) (citiesToAdd !! (index + 1)) link && capacityLinkR tunnels link > 0) links = validTunelR (Reg cities links tunnels) citiesToAdd (index + 1)
+    | any (\link -> validLinksR tunnels link (citiesToAdd !! index) (citiesToAdd !! (index + 1))) links = validTunelR (Reg cities links tunnels) citiesToAdd (index + 1)
     | otherwise = False
 
 
@@ -63,12 +70,15 @@ capacityLinkR tunnels link = (capacityL link) - timesUsed
         timesUsed = length (filter (\tunel -> usesT link tunel) tunnels)
 
 
-citiesToLinksR :: Region -> [ City ] -> [ Link ]
+citiesToLinksR :: Region -> [City] -> [Link]
 citiesToLinksR (Reg cities links tunnels) citiesToAdd
-    | length citiesToAdd <= 1 = [] 
-    | otherwise = validLinks ++ citiesToLinksR (Reg cityTail links tunnels) cityTail
+    | length citiesToAdd == 1 = []
+    | otherwise = llink links city : citiesToLinksR (Reg cityTail links tunnels) cityTail
     where
         city = head citiesToAdd
         cityTail = tail citiesToAdd
-        validLinks = filter (\link -> isValidLink link) links
-        isValidLink link = linksL city (head cityTail) link  && (capacityL link - capacityLinkR tunnels link) > 0
+        llink links city = head(filter (\link -> validLinksR tunnels link city (head cityTail)) links)
+
+
+validLinksR :: [ Tunel ] -> Link -> City -> City -> Bool
+validLinksR tunnels link city1 city2 = linksL city1 city2 link && (capacityLinkR tunnels link) > 0
--- trabajo_practico1/main.hs
+++ trabajo_practico1/main.hs
@@ -26,7 +26,7 @@ city4 = newC "City4" point4
 quality1 :: Quality
 quality1 = newQ "Quality1" 2 1.0
 quality2 :: Quality
-quality2 = newQ "Quality2" 2 2.0
+quality2 = newQ "Quality2" 1 2.0
 
 link1 :: Link
 link1 = newL city1 city2 quality1
@@ -46,6 +46,32 @@ cities :: [ City ]
 cities = [city1, city1]
 
 region1 :: Region
-region1 = Reg [city1, city2, city3, city4] [link1, link2, link3] []
+region1 = newR
 
-region2 = tunelR region1 [city1, city2, city3, city4]
+region2 = foundR region1 city1
+
+region3 = foundR region2 city2
+
+region4 = foundR region3 city3
+
+region5 = foundR region4 city4
+
+region6 = linkR region5 city1 city2 quality1
+
+region7 = linkR region6 city2 city3 quality1
+
+region8 = linkR region7 city3 city4 quality2
+
+region9 = tunelR region8 [city1, city2]
+
+region10 = tunelR region9 [city2, city3, city4]
+
+region11 = connectedR region10 city1 city2
+
+region12 = connectedR region10 city1 city3
+
+region13 = linkedR region10 city1 city4
+
+region14 = delayR region10 city2 city4
+
+region15 = availableCapacityForR region10 city2 city3
